84e5968ad922d4996555a3855207bad121a649d4
Adam Avramov <adam@hack.bg>
2023-08-09 22:50:37 +0300

    refactor: merge stargate and cosmwasm-stargate

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,43 @@
+export { Code, CodeDetails, Contract, ContractCodeHistoryEntry, CosmWasmClient } from "./cosmwasmclient";
+export { fromBinary, toBinary } from "./encoding";
+export { _instantiate2AddressIntermediate, instantiate2Address } from "./instantiate2";
+export {
+  createWasmAminoConverters,
+  isMsgClearAdminEncodeObject,
+  isMsgExecuteEncodeObject,
+  isMsgInstantiateContract2EncodeObject,
+  isMsgInstantiateContractEncodeObject,
+  isMsgMigrateEncodeObject,
+  isMsgStoreCodeEncodeObject,
+  isMsgUpdateAdminEncodeObject,
+  JsonObject,
+  MsgClearAdminEncodeObject,
+  MsgExecuteContractEncodeObject,
+  MsgInstantiateContract2EncodeObject,
+  MsgInstantiateContractEncodeObject,
+  MsgMigrateContractEncodeObject,
+  MsgStoreCodeEncodeObject,
+  MsgUpdateAdminEncodeObject,
+  setupWasmExtension,
+  WasmExtension,
+  wasmTypes,
+} from "./modules";
+export {
+  ChangeAdminResult,
+  ExecuteInstruction,
+  ExecuteResult,
+  InstantiateOptions,
+  InstantiateResult,
+  MigrateResult,
+  SigningCosmWasmClient,
+  SigningCosmWasmClientOptions,
+  UploadResult,
+} from "./signingcosmwasmclient";
+
+// Re-exported because this is part of the CosmWasmClient/SigningCosmWasmClient APIs
+export { Attribute, DeliverTxResponse, Event, IndexedTx } from "@cosmjs/stargate";
+export { HttpEndpoint } from "@cosmjs/tendermint-rpc";
+
 export { Account, accountFromAny, AccountParser } from "./accounts";
 export { AminoConverter, AminoConverters, AminoTypes } from "./aminotypes";
 export { Attribute, Event, fromTendermintEvent } from "./events";
