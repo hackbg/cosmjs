dc12fb1f9ff2e60e47ce953959b20de353a46b59
Adam Avramov <adam@hack.bg>
2023-08-23 02:54:10 +0300

    chore: add cosmjs-types source

---
+++ types/scripts/prepare-publishing.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck >/dev/null && shellcheck "$0"
+
+DIRS=(cosmos cosmos_proto cosmwasm gogoproto google ibc tendermint)
+FILES=(
+  helpers.d.ts
+  helpers.js
+  helpers.js.map
+  index.d.ts
+  index.js
+  index.js.map
+)
+
+for dir in "${DIRS[@]}"; do
+  rm -rf "$dir"
+  cp -R "./build/$dir" ./
+done
+
+for f in "${FILES[@]}"; do
+  rm -rf "$f"
+  cp "./build/$f" ./
+done
