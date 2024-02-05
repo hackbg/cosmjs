e9d7f3f6a19aca3db6b0f25a0340fa09d0ac8cd7
Adam A <adam@hack.bg>
2024-02-05 18:25:54 +0200

    fix(docs): typedoc config

--- typedoc.js
+++ typedoc.js
@@ -1,10 +1,8 @@
-const packageJson = require("./package.json");
-
-module.exports = {
-  entryPoints: ["./src"],
+export default {
+  entryPoints: ["./index.ts"],
   out: "docs",
   exclude: "**/*.spec.ts",
-  name: `${packageJson.name} Documentation`,
+  name: `CosmJS-ESM Documentation`,
   readme: "README.md",
   excludeExternals: true,
   excludePrivate: true,
